peat-psuwit/ubuntu-phone_build

BoardConfig.mk: enabling kernel from the archive (generic/goldfish)
Change-Id: I7980f87a1efe7a34edcf29d7ba85035d593a55e5
Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>

target/board/generic/BoardConfig.mk
@@ -5,7 +5,7 @@
 
 # The generic product target doesn't have any hardware-specific pieces.
 TARGET_NO_BOOTLOADER := true
-TARGET_NO_KERNEL := true
+TARGET_NO_KERNEL := false
 TARGET_ARCH := arm
 
 # Note: we build the platform images for ARMv7-A _without_ NEON.
@@ -74,3 +74,7 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 69206016
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 512
 #TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+
+TARGET_KERNEL_UBUNTU := true
+TARGET_KERNEL_UBUNTU_META := linux-image-goldfish
+TARGET_KERNEL_UBUNTU_SERIES := trusty

target/board/generic_x86/BoardConfig.mk
@@ -5,7 +5,7 @@
 
 # The generic product target doesn't have any hardware-specific pieces.
 TARGET_NO_BOOTLOADER := true
-TARGET_NO_KERNEL := true
+TARGET_NO_KERNEL := false
 TARGET_CPU_ABI := x86
 TARGET_ARCH := x86
 TARGET_ARCH_VARIANT := x86
@@ -41,3 +41,7 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 69206016
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 512
 #TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+
+TARGET_KERNEL_UBUNTU := true
+TARGET_KERNEL_UBUNTU_META := linux-image-goldfish
+TARGET_KERNEL_UBUNTU_SERIES := trusty

Genymobile/f2ut_platform_build

Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

target/board/generic/BoardConfig.mk
@@ -5,7 +5,7 @@
 
 # The generic product target doesn't have any hardware-specific pieces.
 TARGET_NO_BOOTLOADER := true
-TARGET_NO_KERNEL := true
+TARGET_NO_KERNEL := false
 TARGET_ARCH := arm
 
 # Note: we build the platform images for ARMv7-A _without_ NEON.
@@ -92,3 +92,7 @@ ifeq ($(TARGET_PRODUCT),sdk)
   # include an expanded selection of fonts for the SDK.
   EXTENDED_FONT_FOOTPRINT := true
 endif
+
+TARGET_KERNEL_UBUNTU := true
+TARGET_KERNEL_UBUNTU_META := linux-image-goldfish
+TARGET_KERNEL_UBUNTU_SERIES := vivid

target/board/generic_x86/BoardConfig.mk
@@ -5,7 +5,7 @@
 
 # The generic product target doesn't have any hardware-specific pieces.
 TARGET_NO_BOOTLOADER := true
-TARGET_NO_KERNEL := true
+TARGET_NO_KERNEL := false
 TARGET_CPU_ABI := x86
 TARGET_ARCH := x86
 TARGET_ARCH_VARIANT := x86
@@ -58,3 +58,7 @@ BOARD_SEPOLICY_UNION += \
         shell.te \
         system_server.te \
         zygote.te
+
+TARGET_KERNEL_UBUNTU := true
+TARGET_KERNEL_UBUNTU_META := linux-image-goldfish
+TARGET_KERNEL_UBUNTU_SERIES := vivid