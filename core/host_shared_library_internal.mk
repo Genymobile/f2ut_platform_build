410d3ccb3d46b24ba664fc1724f4d3c4ead61940
Marius Gripsgard <mariogrip@ubuntu.com>
2016-03-22 15:54:27 +0100

    Update host_shared_library_internal.mk

--- core/host_shared_library_internal.mk
+++ core/host_shared_library_internal.mk
@@ -24,7 +24,7 @@ endif
 
 $(call host-shared-library-hook)
 
-skip_build_from_source := true
+skip_build_from_source :=
 ifdef LOCAL_PREBUILT_MODULE_FILE
 ifeq (,$(call if-build-from-source,$(LOCAL_MODULE),$(LOCAL_PATH)))
 include $(BUILD_SYSTEM)/prebuilt_internal.mk
